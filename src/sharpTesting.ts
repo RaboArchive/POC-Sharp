85b45affb20d76f62a232744420a099f891d85f0
jordan <j.prudhomme@bee-buzziness.com>
2019-04-05 09:19:09 +0200

    test is ready :)

--- src/sharpTesting.ts
+++ src/sharpTesting.ts
@@ -10,11 +10,17 @@ const sharpTransform = (): any => {
   return sharp().resize(500, 400).jpeg();
 };
 
+// 11 Files & 4 expect to fail
 const files = ["resources/Sample1.jpg",
-  "resources/Sample2.png", "resources/Sample2.jpg", "resources/Sample2.ppm", "resources/Sample2.tga", "resources/Sample2.tif", "resources/Sample2.bmp",
+  "resources/Sample2.png", "resources/Sample2.jpg", "resources/Sample2.ppm", "resources/Sample2.tga", "resources/Sample2.tiff", "resources/Sample2.bmp",
   "resources/Sample3.gif", "resources/Sample3.png", "resources/Sample3.tif", "resources/Sample3.bmp"];
+console.log(`${files.length} files to convert`);
+
 run(() => {
-    wait(fs.remove('output/*'));
+  console.log('Cleaning : output');
+  wait(fs.remove('output'));
+  wait(fs.mkdirp('output'));
+
   console.log('WITHOUT CUSTOM LIBRARY');
   for(const i in files) {
     try {
@@ -22,7 +28,7 @@ run(() => {
       const fileExtension = path.extname(file);
       const fileName = path.basename(file, fileExtension);
       const startTime = Date.now();
-      const outputPath = `output/${fileName}-${fileExtension}-${i}.jpeg`
+      const outputPath = `output/${fileName}-${fileExtension}-${i}.jpeg`;
       let success: boolean = true;
 
       console.log(`Trying to convert : ${file}`);
