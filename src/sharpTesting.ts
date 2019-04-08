5788c8d2f4ae02be28a4f00d5cb0e43bd1173e54
jordan <j.prudhomme@bee-buzziness.com>
2019-04-08 15:57:30 +0200

    working IMM

--- src/sharpTesting.ts
+++ src/sharpTesting.ts
@@ -1,9 +1,9 @@
-import { binaryFileReader, binaryFileWriter, childProcessReader, childProcessWriter, nodeReader, Writer } from 'f-streams';
+import { binaryFileReader, binaryFileWriter, nodeReader } from 'f-streams';
 import { run, wait } from 'f-promise';
 import sharp = require('sharp');
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import * as child_process from "child_process";
+import * as child_process from 'child_process';
 
 
 // 11 Files & 4 expect to fail
@@ -14,103 +14,110 @@ console.log(`${files.length} files to convert`);
 const clean = () => {
     console.log(`Cleaning : output`);
     wait(fs.remove('output'));
-    wait(fs.mkdirp('output/node'));
-    wait(fs.mkdirp('output/fstreams'));
-    wait(fs.mkdirp('output/imageMagick'));
+    wait(fs.mkdirp('output/f'));
+    wait(fs.mkdirp('output/m'));
 };
 
-
-function magick (file: string) {
-    const fileExtension = path.extname(file);
-    const fileName = path.basename(file, fileExtension);
-    const outputPath = `output/imageMagick/${fileName}-${fileExtension}-.jpeg`;
-
-    try {
-        const command = `convert - -thumbnail 400x500 mpr:orig -auto-orient -format JPEG -`;
-        const startTime = Date.now();
-
-        const imageReader = fs.createReadStream(file);
-        // console.log(nodeReader(imageReader).readAll());
-        const proc = child_process.exec(command);
-        imageReader.pipe(proc.stdin);
-        const tmpReader = nodeReader(proc.stdout); //.pipe(imageWriter.nodify());
-        tmpReader.pipe(binaryFileWriter(outputPath));
-        console.log(` ${file} - Done - in ${(Date.now() - startTime) / 1000}s`);
-    } catch (e) {
-        // console.log(`[${libUsed}] Err : Catched while doing things to ${files[i]}`);
-        wait(fs.remove(outputPath));
-    }
+interface IImage {
+    file: string;
+    fileExtension: string;
+    fileName: string;
+    outputPath: string;
 }
-run(() => {
-    if (process.argv[2] === 'node') {
 
-        clean();
-        const libUsed = 'NODEJS';
-        const sharpTransform = (): any => {
-            return sharp().resize(500, 400).jpeg({
-                quality: 80,
-                progressive: true,
-            });
+
+const magick = (image: IImage) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const command = `convert ${image.file} -thumbnail 500x400 mpr:orig -auto-orient output/m/${image.outputPath}`;
+            child_process.exec(command)
+                .on('exit', resolve);
+        } catch (e) {
+            console.log(e);
+            wait(fs.remove(image.outputPath));
+            reject();
+        }
+    });
+}
+;
+
+run(() => {
+    clean();
+    for (const i in files) {
+        // CONFIG
+        const file = files[i];
+        const fileExtension = path.extname(file);
+        const fileName = path.basename(file, fileExtension);
+        const outputPath = `${fileName}-${fileExtension}-${i}.jpeg`;
+
+        const image: IImage = {
+            file,
+            fileExtension,
+            fileName,
+            outputPath,
         };
 
-        for (const i in files) {
-            const file = files[i];
-            const fileExtension = path.extname(file);
-            const fileName = path.basename(file, fileExtension);
-            const outputPath = `output/node/${fileName}-${fileExtension}-${i}.jpeg`;
-            let success: boolean = true;
-            try {
-                console.log(`[${libUsed}] Trying to convert : ${file} to ${outputPath}`);
-                const startTime = Date.now();
-                const outputStream = fs.createWriteStream(outputPath);
-                fs.createReadStream(file)
-                    .pipe(sharpTransform())
-                    .on('error', () => {
-                        console.log(`[${libUsed}] Caught error in ${file} convertion`);
-                        success = false;
-                        fs.remove(outputPath);
-                    })
-                    .pipe(outputStream)
-                    .on('close', () => {
-                        if (success) {
-                            console.log(`[${libUsed}] ${file} - Done - in ${(Date.now() - startTime) / 1000}s`);
-                        }
-                    });
-            } catch (e) {
-                console.log('[${libUsed}] Err : Catched while doing things', e, e.stack);
-            }
-        }
-    } else if (process.argv[2] === 'f') {
-        clean();
-        const libUsed = 'F-STREAMS';
-        for (const i in files) {
-            const file = files[i];
-            const fileExtension = path.extname(file);
-            const fileName = path.basename(file, fileExtension);
-            const outputPath = `output/fstreams/${fileName}-${fileExtension}-${i}.jpeg`;
-            const transformer = sharp().rotate().resize(500, 400).jpeg({
-                quality: 80,
-                progressive: true,
-            });
-                const startTime = Date.now();
-            try {
-                // console.log(`[${libUsed}] Trying to convert : ${file} to ${outputPath}`);
+        const transformer = sharp().rotate().resize(500, 400).jpeg({
+            quality: 80,
+            progressive: true,
+        });
 
-                const imageReader = binaryFileReader(file);
-                const outputReader = imageReader.nodeTransform(transformer);
-                const imageWriter = binaryFileWriter(outputPath);
-                outputReader.pipe(imageWriter);
-
-                console.log(`[${libUsed}] ${file} - [SHARP]] - in ${(Date.now() - startTime) / 1000}s`);
-                magick(file);
-                console.log(`[${libUsed}] ${file} - [IMM] - in ${(Date.now() - startTime) / 1000}s`);
-            } catch (e) {
-                magick(file);
-                console.log(`[${libUsed}] ${file} - [__IMM] - in ${(Date.now() - startTime) / 1000}s`);
-            }
+        // Timer start
+        const startTimeSharp = Date.now();
+        try {
+            binaryFileReader(image.file)
+                .nodeTransform(transformer)
+                .pipe(binaryFileWriter(`output/f/${image.outputPath}`));
+            console.log(`[SHARP] - ${image.fileName}${image.fileExtension} in ${(Date.now() - startTimeSharp) / 1000}s`);
+            const startTimeIMM = Date.now();
+            wait(magick(image));
+            console.log(`[IMM] - ${image.fileName}${image.fileExtension} - in ${(Date.now() - startTimeIMM) / 1000}s\n`);
+        } catch (e) {
+            wait(magick(image));
+            console.log(`\n[FALLBACK_IMM] - ${image.fileName}${image.fileExtension} - in ${(Date.now() - startTimeSharp) / 1000}s\n`);
         }
     }
 });
+
+
+// Useless
+// const node = () => {
+//     clean();
+//     const libUsed = 'NODEJS';
+//     const sharpTransform = (): any => {
+//         return sharp().resize(500, 400).jpeg({
+//             quality: 80,
+//             progressive: true,
+//         });
+//     };
+//     for (const i in files) {
+//         const file = files[i];
+//         const fileExtension = path.extname(file);
+//         const fileName = path.basename(file, fileExtension);
+//         const outputPath = `output/node/${fileName}-${fileExtension}-${i}.jpeg`;
+//         let success: boolean = true;
+//         try {
+//             console.log(`[${libUsed}] Trying to convert : ${file} to ${outputPath}`);
+//             const startTime = Date.now();
+//             const outputStream = fs.createWriteStream(outputPath);
+//             fs.createReadStream(file)
+//                 .pipe(sharpTransform())
+//                 .on('error', () => {
+//                     console.log(`[${libUsed}] Caught error in ${file} convertion`);
+//                     success = false;
+//                     fs.remove(outputPath);
+//                 })
+//                 .pipe(outputStream)
+//                 .on('close', () => {
+//                     if (success) {
+//                         console.log(`[${libUsed}] ${file} - Done - in ${(Date.now() - startTime) / 1000}s`);
+//                     }
+//                 });
+//         } catch (e) {
+//             console.log('[${libUsed}] Err : Catched while doing things', e, e.stack);
+//         }
+//     }
+// };
 
 
 
